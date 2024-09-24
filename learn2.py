f4d518e01bbe9b6ac9f877e69fe96cedfd32ee16
Kirill.dll <bobbikcat@gmail.com>
2024-09-24 15:03:49 -0400

    Initial commit

---
+++ learn2.py
@@ -0,0 +1,40 @@
+a = 4
+b = 2
+
+ab_result = a + b
+print(ab_result)
+ab_result = a - b
+print(ab_result)
+print()
+
+x = 10
+y = 5
+
+xy_result = x * y
+print(xy_result)
+xy_result = x / y
+print(xy_result)
+print()
+
+m = 21
+n = 3
+
+mn_result = m // n
+print(mn_result)
+mn_result = m % n
+print(mn_result)
+print()
+
+base = 6
+exponent = 2
+
+be_result = 6 ** 2
+print(be_result)
+print()
+
+abc_result = (5 + 3) * 2
+print(abc_result)
+abc_result = 10 / 3 + 7
+print(abc_result)
+abc_result = 8 ** 2 - 5
+print(abc_result)
